50b3fccd30dca7e2b5be708c631d028499f61ec3
Mareel <maree.lazarides@gmail.com>
2017-12-28 14:47:36 +1000

    added imgs and js and general tidy up for publishing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".photo-showing").toggle();
-    $(".photo-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#march-showing").toggle();
+    $("#march-hidden").toggle();
   });
+});
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#may-showing").toggle();
+    $("#may-hidden").toggle();
+  });
+});
+
+$(".clickable").click(function() {
+  $("#photo-showing").toggle();
+  $("#photo-hidden").toggle();
 });
